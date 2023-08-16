1d15b1ecbee47bc46faf30558c6949da3fab71d7
Lokesh9177 <137813665+Lokesh9177@users.noreply.github.com>
2023-08-16 15:46:56 +0530

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,2 +1,16 @@
-#!/bin/bash
-echo "Hello World !!!!"
+# <YOUR ACCOUNT NAME>
+schemaVersion: vCurrent
+clusters:
+  - name: mybuilds
+    region: <YOUR REGION>
+    queueName: <YOUR-QUEUE-NAME>:cicd
+    gitRepositories:
+      - <THE SSH ADDRESS OF YOUR REPO>
+    network:
+      managedNetworkName: <THE MANAGED NETWORK YOUR ACCOUNT IS ENROLLED IN>
+    instance:
+      instanceSize: small
+    managedPolicyArns:
+      - <YOUR POLICY ARN>
+    autoScaling:
+      maxInstanceCount: 2
